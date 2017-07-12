cbcc020b437372cd30b003870f7df2830501dcf6
myapham <myapham0410@gmail.com>
2017-07-12 17:47:49 -0400

    Added PredeploymentScript. [#148792861]

---
+++ HousingManager.DB/HousingManager.DB.SqlServer/Person/Provider.sql
@@ -0,0 +1,6 @@
+﻿Create table Person.Provider(
+	ProviderId int IDENTITY(1,1) not null,
+	Name nvarchar(50) not null,
+	constraint PK_Person_Provider_ProviderId primary key clustered (ProviderId),
+    
+);
---
+++ HousingManager.DB/HousingManager.DB.SqlServer/Person/ProviderContactInfo.sql
@@ -0,0 +1,9 @@
+﻿Create table Person.ProviderContactInfo(
+	ProviderId int not null, 
+	ContactInfoId int not null,
+	constraint PK_Person_ProviderContactInfo_ProviderId_ContactInfoId primary key clustered (ProviderId, ContactInfoId),
+	CONSTRAINT FK_Person_ProviderContactInfo_ProviderId FOREIGN KEY (ProviderId) REFERENCES Person.Provider(ProviderId),
+	CONSTRAINT FK_Person_ProviderContactInfo_ContactInfoId FOREIGN KEY (ContactInfoId) REFERENCES Person.ContactInfo(ContactInfoId),
+
+	
+);
