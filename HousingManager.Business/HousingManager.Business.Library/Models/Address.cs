9e1c0ac237d8e03e830e517a7c9ec860a4cd8e3b
andre ballard <andre.ballard@outlook.com>
2017-07-12 18:17:07 -0400

    Created an address class with streetname, apartment number, city, state, and zip code properties. [#148793863]

---
+++ HousingManager.Business/HousingManager.Business.Library/Models/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingManager.Business.Library.Models
+{
+  public class Address
+  {
+    public string StreetName { get; set; }
+    public string AptNumber { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string ZipCode { get; set; }
+  }
+}
