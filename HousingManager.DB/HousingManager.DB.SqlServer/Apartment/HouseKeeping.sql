13469e062b34671ba98e970a7b1fe58b55681d14
myapham <myapham0410@gmail.com>
2017-07-13 00:48:35 -0400

    Changed MaintenanceRequest table under Request schema. Changed MaintenanceRequestID to MaintenanceRequestId [#148792861]

--- HousingManager.DB/HousingManager.DB.SqlServer/Apartment/HouseKeeping.sql
+++ HousingManager.DB/HousingManager.DB.SqlServer/Apartment/HouseKeeping.sql
@@ -1,6 +1,6 @@
 ﻿create table Apartment.HouseKeeping(
     HousekeepingId int IDENTITY(1,1) not null,
-    ApartmentUnitId int unique not null,
+    ApartmentUnitId int unique NOT null,
     LastCleaning date not null,
 	NextCleaning date not null,
 	constraint PK_Apartment_HouseKeeping_HouseKeepingId primary key clustered (HousekeepingId),
--- HousingManager.DB/HousingManager.DB.SqlServer/Person/Person.sql
+++ HousingManager.DB/HousingManager.DB.SqlServer/Person/Person.sql
@@ -2,6 +2,6 @@
 Create table Person.Person(
 	PersonId int PRIMARY KEY CLUSTERED IDENTITY(1,1) not null,
     FirstName nvarchar(50) not null,
-    LastName nvarchar(50) not null,
+    LastName nvarchar(50) NOT null,
     Active bit default(1)	
 );
