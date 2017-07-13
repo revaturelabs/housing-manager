bf3ef8783b8058078cb236a94d0d2904f2f582e3
myapham <myapham0410@gmail.com>
2017-07-13 00:12:18 -0400

    Added MaintenanceType table under Request schema. [#148792861]

---
+++ HousingManager.DB/HousingManager.DB.SqlServer/Request/MaintenanceType.sql
@@ -0,0 +1,5 @@
+﻿create table Request.MaintenanceType(
+    MaintenanceTypeId int IDENTITY(1,1) not null,
+    Issue nvarchar(50) not null,
+	constraint PK_Request_MaintenanceRequest_MaintenanceTypeId primary key clustered (MaintenanceTypeId),
+);
