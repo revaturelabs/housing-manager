3bc77b4acb468fecd4e9c9449b066c21e4c72b04
myapham <myapham0410@gmail.com>
2017-07-13 00:10:44 -0400

    Addede MaintenanceRequest table under Request schema. [#148792861]

---
+++ HousingManager.DB/HousingManager.DB.SqlServer/Request/MaintenanceRequest.sql
@@ -0,0 +1,13 @@
+﻿create table Request.MaintenanceRequest(
+    MaintenanceRequestId int IDENTITY(1,1) not null,
+    MaintenanceTypeID int not null,
+    ApartmentUnitId int not null,
+	ManagerId int not null,
+	Submitted datetime not null,
+	Desctiption nvarchar not null,
+	constraint PK_Request_MaintenanceRequest_MaintenanceRequestId primary key clustered (MaintenanceRequestId),
+	CONSTRAINT FK_Request_MaintenanceRequest_MaintenanceRequestTypeID FOREIGN KEY (MaintenanceTypeID) REFERENCES Request.MaintenanceType(MaintenanceTypeID),
+	CONSTRAINT FK_Request_MaintenanceRequest_ApartmentUnitId FOREIGN KEY (ApartmentUnitId) REFERENCES Request.MaintenanceType(MaintenanceTypeID),
+
+  
+);
