5dd6559df34a546748d3553a98ee745bb43e510b
myapham <myapham0410@gmail.com>
2017-07-12 23:48:12 -0400

    Added BatchType table under Batch schema. [#148792861]

---
+++ HousingManager.DB/HousingManager.DB.SqlServer/Batch/BatchType.sql
@@ -0,0 +1,5 @@
+﻿create table Batch.BatchType(
+    BatchTypeId int IDENTITY(1,1) not null,
+    Type nvarchar(50) not null,
+	constraint PK_Batch_BatchType_BatchTypeId primary key clustered (BatchTypeId),
+);
