d3fc5ec43345e294efaa96fdb128edc5f36a16bf
myapham <myapham0410@gmail.com>
2017-07-12 23:37:56 -0400

    Added ApartmentUnit under Apartment schema [#148792861]

---
+++ HousingManager.DB/HousingManager.DB.SqlServer/Apartment/ApartmentUnit.sql
@@ -0,0 +1,11 @@
+﻿create table Apartment.ApartmentUnit(
+    ApartmentUnitId int IDENTITY(1,1) not null,
+    ApartmentComplexId int not null,
+    AddressId int unique not null,
+    ProviderId int not null,
+    Capacity int not null,
+	constraint PK_Apartment_ApartmentUnit_ApartmentUnitId primary key clustered (ApartmentUnitId),
+		CONSTRAINT FK_Apartment_ApartmentUnit_ApartmentComplexId FOREIGN KEY (ApartmentComplexId) REFERENCES Apartment.ApartmentComplex(ApartmentComplexId),
+			CONSTRAINT FK_Apartment_ApartmentUnit_ApartmentId FOREIGN KEY (AddressId) REFERENCES Apartment.Address(AddressId),
+			CONSTRAINT FK_Apartment_ApartmentUnit_ProviderId FOREIGN KEY (ProviderId) REFERENCES Person.Provider(ProviderId)
+);
