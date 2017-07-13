c55dd346013201de5fdf4eeb7a5d436ea6768696
andre ballard <andre.ballard@outlook.com>
2017-07-13 16:12:52 -0400

    Created ContactInfo model with email address and phone number properties. [#148829323]

---
+++ HousingManager.Business/HousingManager.Business.Library/Models/ContactInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingManager.Business.Library.Models
+{
+    public class ContactInfo
+    {
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
