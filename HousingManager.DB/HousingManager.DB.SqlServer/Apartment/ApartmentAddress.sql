5c65026333867397b87e6b1037b12b56168253bf
myapham <myapham0410@gmail.com>
2017-07-12 23:41:56 -0400

    Added Address table under Apartment schema [#148792861]

---
+++ HousingManager.DB/HousingManager.DB.SqlServer/Apartment/ApartmentAddress.sql
@@ -0,0 +1,10 @@
+﻿
+create table Apartment.Address(
+    AddressId int IDENTITY(1,1) not null,
+    StreetName nvarchar(50) not null,
+    AptNum nvarchar not null,
+    City nvarchar(30)  not null,
+    State nvarchar(10) not null,
+	ZipCode int not null,
+	constraint PK_Apartment_Address_AddressId primary key clustered (AddressId),
+);
