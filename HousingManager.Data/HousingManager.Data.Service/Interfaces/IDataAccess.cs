79f2a761cbdd3d50401285492589f4b346d8d216
jameson burchette <jameson.burchette@outlook.com>
2017-07-13 15:25:21 -0400

    add IDataAccess interface with CRUD method signatures [#148857999]

---
+++ HousingManager.Data/HousingManager.Data.Service/Interfaces/IDataAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingManager.Data.Service.Interfaces
+{
+    public interface IDataAccess<T>
+    {
+        T Create(T model);
+        List<T> Read();
+        T Update(T model);
+        bool Delete(T model);
+    }
+}
