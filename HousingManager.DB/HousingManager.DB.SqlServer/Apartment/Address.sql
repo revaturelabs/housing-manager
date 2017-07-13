861223629ea45635bda6d0ca0b9199a62c168a35
myapham <myapham0410@gmail.com>
2017-07-13 14:53:03 -0400

    tested insert for address table [delivers #148851335]

--- HousingManager.DB/HousingManager.DB.SqlServer/Apartment/Address.sql
+++ HousingManager.DB/HousingManager.DB.SqlServer/Apartment/Address.sql
@@ -1,9 +1,8 @@
 ﻿create table Apartment.Address(
-    AddressId int IDENTITY(1,1) not null,
-    StreetName nvarchar(50) not null,
-    AptNum nvarchar not null,
+    AddressId int primary key clustered IDENTITY(1,1) not null,
+    StreetName nvarchar(200) not null,
+    AptNum nvarchar(5) not null,
     City nvarchar(30)  not null,
-    State nvarchar(10) not null,
-	ZipCode int not null,
-	constraint PK_Apartment_Address_AddressId primary key clustered (AddressId),
+    [State] nvarchar(10) not null,
+	ZipCode int not null
 );
--- HousingManager.DB/HousingManager.DB.SqlServer/TestRun.sql
+++ HousingManager.DB/HousingManager.DB.SqlServer/TestRun.sql
@@ -10,3 +10,12 @@ GO
 SELECT * FROM sys.databases;
 
 SELECT * FROM sys.schemas;
+
+
+USE HousingManagerDB;
+GO
+
+SELECT * FROM Apartment.Address;
+
+INSERT INTO Apartment.Address(StreetName, AptNum, City, [State], ZipCode)
+values ('2121 Raven Tower Court', '504', 'Herndon', 'VA', 20170);
