aad4c791a4ebcdc68dedc2ce60e01dc28fb0a7b8
myapham <myapham0410@gmail.com>
2017-07-13 00:06:28 -0400

    Added ApartmentRequest table under Request schema.[#148792861]

---
+++ HousingManager.DB/HousingManager.DB.SqlServer/Request/ApartmentRequest.sql
@@ -0,0 +1,13 @@
+﻿Create table Request.ApartmentRequest(
+	ApartmentRequestId int IDENTITY(1,1)not null,
+	ManagerId int not null, 
+	ProviderId int not null, 
+	Submitted datetime,
+	Beds int not null
+		constraint PK_Request_ApartmentRequest_ApartmentRequestId primary key clustered (ApartmentRequestId),
+		CONSTRAINT FK_Request_ApartmentRequest_ManagerId FOREIGN KEY (ManagerId) REFERENCES Person.Person(PersonId),
+		CONSTRAINT FK_Request_ApartmentRequest_ManagerId FOREIGN KEY (ProviderId) REFERENCES Person.Provider(ProviderId),
+	
+			
+
+);
