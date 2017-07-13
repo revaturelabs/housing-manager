245b7b68af7c4af56f05505e107ff119c26566d0
myapham <myapham0410@gmail.com>
2017-07-12 23:35:13 -0400

    Added ApartmentComplex table under Apartment scheme [#148792861]

---
+++ HousingManager.DB/HousingManager.DB.SqlServer/Person/ContactInfo.sql
@@ -0,0 +1,7 @@
+﻿Create table Person.ContactInfo(
+	ContactInfoId int IDENTITY(1,1) not null,
+    EmailAddress nvarchar(50) not null,
+    PhoneNumber nvarchar(50) not null,
+	constraint PK_Person_ContactInfo_ContactInfoId primary key clustered (ContactInfoId),
+   
+);
