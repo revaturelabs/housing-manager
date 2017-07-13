1a3bf97ccda0edcf2d002e5875f5802f405fbc3b
jameson burchette <jameson.burchette@outlook.com>
2017-07-13 15:53:02 -0400

    Create interface IDataFactory that will define the methods that DataFactories should have. [#148857999]

---
+++ HousingManager.Data/HousingManager.Data.Service/Interfaces/IDataFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingManager.Data.Service.Interfaces
+{
+    public interface IDataFactory
+    {
+        IDataAccess<Address> GetAddressDataAccessEntity();
+    }
+}
