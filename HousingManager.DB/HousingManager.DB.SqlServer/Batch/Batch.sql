55231935032432ac8f992d1be4c8fe08513a1b04
myapham <myapham0410@gmail.com>
2017-07-14 14:25:40 -0400

    Added constraints for primary key and foreign key [delivers #148926057]

--- HousingManager.DB/HousingManager.DB.SqlServer/Batch/Batch.sql
+++ HousingManager.DB/HousingManager.DB.SqlServer/Batch/Batch.sql
@@ -3,6 +3,7 @@
     BatchTypeId int not null,
     StarDate DateTime not null,
 	EndDate DateTime not null,
+
 	CONSTRAINT FK_Batch_Batch_BatchType FOREIGN KEY (BatchTypeId) REFERENCES Batch.BatchType(BatchTypeId)
 
 );
