537470ee5090a918bf5ea8f2a77a837686eb1734
jameson burchette <jameson.burchette@outlook.com>
2017-07-14 15:17:41 -0400

    removed my port from data services's url in the business service after testing [delivers #148926065]

--- HousingManager.Business/HousingManager.Business.Service/Brokers/DataBroker.cs
+++ HousingManager.Business/HousingManager.Business.Service/Brokers/DataBroker.cs
@@ -10,7 +10,7 @@ namespace HousingManager.Business.Service.Brokers
 {
     public class DataBroker
     {
-        private static string _Url = "";
+        private static string _Url = "http://localhost/";
 
         private HttpClient _Client = new HttpClient();
 
