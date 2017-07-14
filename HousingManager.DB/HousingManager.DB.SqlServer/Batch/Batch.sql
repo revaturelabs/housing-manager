391852e64a1a1135e3775f52f219455e35fb330a
myapham <myapham0410@gmail.com>
2017-07-14 14:23:42 -0400

    Added Batch table under Batch schema. [#148926057]

---
+++ HousingManager.DB/HousingManager.DB.SqlServer/Batch/Batch.sql
@@ -0,0 +1,8 @@
+﻿create table Batch.Batch(
+    BatchId int primary key clustered IDENTITY(1,1) not null,
+    BatchTypeId int not null,
+    StarDate DateTime not null,
+	EndDate DateTime not null,
+	CONSTRAINT FK_Batch_Batch_BatchType FOREIGN KEY (BatchTypeId) REFERENCES Batch.BatchType(BatchTypeId)
+
+);
