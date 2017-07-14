4cdb2b372ce289ca53e8cf79260c475c8e9102d5
andre ballard <andre.ballard@outlook.com>
2017-07-14 15:22:48 -0400

    Created first name, last name, address, gender, and contact information properties in Person model. [#148830107]

--- HousingManager.Business/HousingManager.Business.Library/Models/Person.cs
+++ HousingManager.Business/HousingManager.Business.Library/Models/Person.cs
@@ -4,8 +4,12 @@ using System.Text;
 
 namespace HousingManager.Business.Library.Models
 {
-    public class Person
-    {
-      
-    }
+  public class Person
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public Address PersonAddress { get; set; }
+    public char Gender { get; set; }
+    public ContactInfo ContactInformation { get; set; }
+  }
 }
