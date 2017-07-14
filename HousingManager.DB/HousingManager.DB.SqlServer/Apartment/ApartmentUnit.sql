77a32cff94b00f84a39f07789a5eed0b00d52c53
myapham <myapham0410@gmail.com>
2017-07-14 15:12:14 -0400

    Added primary key and foreign key to apartmentunit table [delivers #148929907]

--- HousingManager.DB/HousingManager.DB.SqlServer/Apartment/ApartmentUnit.sql
+++ HousingManager.DB/HousingManager.DB.SqlServer/Apartment/ApartmentUnit.sql
@@ -4,6 +4,7 @@
     AddressId int unique not null,
     ProviderId int not null,
     Capacity int not null,
+		--constraint PK_Apartment_ApartmentUnit_ApartmentUnitId primary key clustered (ApartmentUnitId),
 		CONSTRAINT FK_Apartment_ApartmentUnit_ApartmentComplexId FOREIGN KEY (ApartmentComplexId) REFERENCES Apartment.ApartmentComplex(ApartmentComplexId),
 		CONSTRAINT FK_Apartment_ApartmentUnit_ApartmentId FOREIGN KEY (AddressId) REFERENCES Apartment.Address(AddressId),
 		CONSTRAINT FK_Apartment_ApartmentUnit_ProviderId FOREIGN KEY (ProviderId) REFERENCES Person.Provider(ProviderId)
