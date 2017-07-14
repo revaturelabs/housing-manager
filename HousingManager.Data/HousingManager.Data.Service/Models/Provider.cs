800bd89d153c5e995862c294d93f25483b5519cb
Ryan Pacovsky <Ryan.Pacovsky@gmail.com>
2017-07-14 15:44:38 -0400

    Created the Provider model. [#148924987]

---
+++ HousingManager.Data/HousingManager.Data.Service/Models/Provider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingManager.Data.Service.Models
+{
+    public class Provider
+    {
+        public string Name { get; set; }
+        public ContactInfo ProviderContactInfo { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null && obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            var o = obj as Provider;
+
+            return (o.Name == Name && o.ProviderContactInfo == ProviderContactInfo);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Name.GetHashCode() + ProviderContactInfo.GetHashCode());
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public ContactInfo GetProviderContactInfo()
+        {
+            return ProviderContactInfo;
+        }
+    }
+}
