ec38d538099f83fc6890533fc3ce6fa955cd3f7b
jameson burchette <jameson.burchette@outlook.com>
2017-07-13 20:51:32 -0400

    add GetValues method to get data from the data service's values controller [#148865401]

--- HousingManager.Business/HousingManager.Business.Service/Brokers/DataBroker.cs
+++ HousingManager.Business/HousingManager.Business.Service/Brokers/DataBroker.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace HousingManager.Business.Service.Brokers
 {
     public class DataBroker
     {
+        private static string _Url = "";
+
+        private HttpClient _Client = new HttpClient();
+
+        public string GetValues()
+        {
+            var res = _Client.GetAsync(_Url + "/api/values").Result;
+            if (res.IsSuccessStatusCode)
+            {
+                return res.Content.ReadAsStringAsync().Result;
+            }
+            else
+            {
+                return "error";
+            }
+        }
     }
 }
