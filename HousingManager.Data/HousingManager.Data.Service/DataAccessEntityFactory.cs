9540308c7171b88277225f52984e418bc67caf92
jameson burchette <jameson.burchette@outlook.com>
2017-07-14 10:14:26 -0400

    add changed context name and added project reference and using statement for data library's entity framework [finishes #148857999]

--- HousingManager.Data/HousingManager.Data.Service/DataAccessEntityFactory.cs
+++ HousingManager.Data/HousingManager.Data.Service/DataAccessEntityFactory.cs
@@ -4,16 +4,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HousingManager.Data.Library.Model1;
 
 namespace HousingManager.Data.Service
 {
     public class DataAccessEntityFactory : IDataAccessFactory
     {
-        private static DbContext _Context = new HousingManagerDBEntities();
+        private static DbContext _Context = new HousingManagerDBContext();
 
         public IDataAccess<Address> GetAddressDAO()
         {
-            return new DataAccessEntity<Address>(_Context.Addresses);
+            return new DataAccessEntity<Address>(_Context.Address);
         }
     }
 }
