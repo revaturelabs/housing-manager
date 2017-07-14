8b9c0ae137a0e2aecb545c7019be8ae59e7cb643
Ryan Pacovsky <Ryan.Pacovsky@gmail.com>
2017-07-14 14:46:10 -0400

    Created the ContactInfo data model. [#148925435]

---
+++ HousingManager.Data/HousingManager.Data.Service/Models/ContactInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingManager.Data.Service.Models
+{
+    public class ContactInfo
+    {
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
