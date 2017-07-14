d8c484dcac7a89c13d0476d3b4b5c03c70b8262d
Ryan Pacovsky <Ryan.Pacovsky@gmail.com>
2017-07-14 15:09:19 -0400

    Created the test class for ContactInfo. [delivers #148925435]

--- HousingManager.Data/HousingManager.Data.Service/Models/ContactInfo.cs
+++ HousingManager.Data/HousingManager.Data.Service/Models/ContactInfo.cs
@@ -9,5 +9,32 @@ namespace HousingManager.Data.Service.Models
     {
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null && obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            var o = obj as ContactInfo;
+
+            return (o.Email == Email && o.PhoneNumber == PhoneNumber);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Email.GetHashCode() + PhoneNumber.GetHashCode());
+        }
+
+        public string GetEmail()
+        {
+            return Email;
+        }
+
+        public string GetPhoneNumber()
+        {
+            return PhoneNumber;
+        }
     }
 }
