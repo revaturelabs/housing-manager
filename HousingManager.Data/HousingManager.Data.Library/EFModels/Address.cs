2c8be15682cf6710c8fbed9b0b153ba5c6020257
jameson burchette <jameson.burchette@outlook.com>
2017-07-14 13:19:20 -0400

    generated database models with entity framework [finishes #148921509]

---
+++ HousingManager.Data/HousingManager.Data.Library/EFModels/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HousingManager.Data.Library.EFModels
+{
+    public partial class Address
+    {
+        public int AddressId { get; set; }
+        public string StreetName { get; set; }
+        public string AptNum { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+    }
+}
---
+++ HousingManager.Data/HousingManager.Data.Library/EFModels/HousingManager_DB_SqlServerContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace HousingManager.Data.Library.EFModels
+{
+    public partial class HousingManager_DB_SqlServerContext : DbContext
+    {
+        public virtual DbSet<Address> Address { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\ProjectsV13;Database=HousingManager.DB.SqlServer;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Address>(entity =>
+            {
+                entity.ToTable("Address", "Apartment");
+
+                entity.Property(e => e.AptNum)
+                    .IsRequired()
+                    .HasMaxLength(5);
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.Property(e => e.State)
+                    .IsRequired()
+                    .HasMaxLength(10);
+
+                entity.Property(e => e.StreetName)
+                    .IsRequired()
+                    .HasMaxLength(200);
+            });
+        }
+    }
+}
