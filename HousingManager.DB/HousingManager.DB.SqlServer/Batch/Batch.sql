a6172fba17163b3d518be3d0a89ee23955f961de
myapham <myapham0410@gmail.com>
2017-07-14 17:06:43 -0400

    Updated Batch table, Size added to the table [delivers #148926057]

--- HousingManager.DB/HousingManager.DB.SqlServer/Batch/Batch.sql
+++ HousingManager.DB/HousingManager.DB.SqlServer/Batch/Batch.sql
@@ -3,6 +3,7 @@
     BatchTypeId int not null,
     StarDate DateTime not null,
 	EndDate DateTime not null,
+	Size int not null,
 
 	CONSTRAINT FK_Batch_Batch_BatchType FOREIGN KEY (BatchTypeId) REFERENCES Batch.BatchType(BatchTypeId)
 
