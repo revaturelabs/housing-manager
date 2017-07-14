33ffee39b73b0f1a52c8e84ba5b5c1931fe31d87
andre ballard <andre.ballard@outlook.com>
2017-07-14 14:20:06 -0400

    Created equals method for Address model. Created positive test for equals method. [#148922283]

--- HousingManager.Business/HousingManager.Business.Tests/AddressTest.cs
+++ HousingManager.Business/HousingManager.Business.Tests/AddressTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using NUnit.Framework;
 using HousingManager.Business.Library;
+using HousingManager.Business.Library.Models;
 
 namespace HousingManager.Business.Tests
 {
@@ -24,5 +25,28 @@ namespace HousingManager.Business.Tests
             var address = a.AddAddress2();
             Assert.IsNull(address);
         }
+
+    [Test]
+    public void ContactInfoIsEqualPositive()
+    {
+      var c = new Address();
+      c.AptNumber = "112";
+      c.StreetName = "222 somewhere else";
+      c.City = "Houston";
+      c.State = "Texas";
+      c.ZipCode = "11111";
+
+      var c1 = new Address();
+      c1.AptNumber = "112";
+      c1.StreetName = "222 somewhere else";
+      c1.City = "Houston";
+      c1.State = "Texas";
+      c1.ZipCode = "11111";
+
+      var chash = c.GetHashCode();
+      var c1hash = c1.GetHashCode();
+
+      Assert.IsTrue(c.Equals(c1) && chash == c1hash);
+    }
   }
 }
