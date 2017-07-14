deccbdc9a5baff7dc3b025e722b08e1b605ad3b8
andre ballard <andre.ballard@outlook.com>
2017-07-14 14:26:50 -0400

    Created negative test for equals method. [#148922283]

--- HousingManager.Business/HousingManager.Business.Tests/AddressTest.cs
+++ HousingManager.Business/HousingManager.Business.Tests/AddressTest.cs
@@ -48,5 +48,28 @@ namespace HousingManager.Business.Tests
 
       Assert.IsTrue(c.Equals(c1) && chash == c1hash);
     }
+
+    [Test]
+    public void ContactInfoIsEqualNegative()
+    {
+      var c = new Address();
+      c.AptNumber = "112";
+      c.StreetName = "222 somewhere else";
+      c.City = "Houston";
+      c.State = "Texas";
+      c.ZipCode = "11111";
+
+      var c1 = new Address();
+      c1.AptNumber = "112";
+      c1.StreetName = "222 somewhere else";
+      c1.City = "Kingsville";
+      c1.State = "Texas";
+      c1.ZipCode = "11111";
+
+      var chash = c.GetHashCode();
+      var c1hash = c1.GetHashCode();
+
+      Assert.IsFalse(c.Equals(c1) && chash == c1hash);
+    }
   }
 }
