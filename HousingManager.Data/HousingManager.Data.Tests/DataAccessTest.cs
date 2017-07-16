aba3d07a46aeaea11b574dc408ba0a2e3886d01a
jameson burchette <jameson.burchette@outlook.com>
2017-07-16 14:23:35 -0400

    add read test to data access test class [#148966557]

--- HousingManager.Data/HousingManager.Data.Tests/DataAccessTest.cs
+++ HousingManager.Data/HousingManager.Data.Tests/DataAccessTest.cs
@@ -4,6 +4,7 @@ using HousingManager.Data.Service.Interfaces;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace HousingManager.Data.Tests
@@ -11,18 +12,26 @@ namespace HousingManager.Data.Tests
     [TestFixture]
     public class DataAccessTest
     {
+        private static string _ConString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=HousingManager.DB.SqlServer;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+
         private IDataAccess<Address> _AdrData = new DataAccessEntityFactory().GetAddressDAO();
 
         [SetUp]
         public void SetUp()
         {
-            
+            new SqlCommand("test_Setup", new SqlConnection(_ConString)).ExecuteNonQuery();
         }
 
         [TearDown]
         public void TearDown()
         {
+            new SqlCommand("test_Teardown", new SqlConnection(_ConString)).ExecuteNonQuery();
+        }
 
+        [Test]
+        public void ReadTest()
+        {
+            Assert.IsTrue(_AdrData.Read().Count >= 1);
         }
     }
 }
