dc89cb2e0414bc3c9cfe149397cd5bad026a7693
myapham <myapham0410@gmail.com>
2017-07-16 17:37:22 -0400

    Added PK constraint to contactinfo table under person schema [delivers #148856741]

--- HousingManager.DB/HousingManager.DB.SqlServer/Person/ContactInfo.sql
+++ HousingManager.DB/HousingManager.DB.SqlServer/Person/ContactInfo.sql
@@ -3,5 +3,4 @@ Create table Person.ContactInfo(
 	ContactInfoId int primary key clustered IDENTITY(1,1) not null,
     EmailAddress nvarchar(50) not null,
     PhoneNumber nvarchar(50) not null
-   
 );
