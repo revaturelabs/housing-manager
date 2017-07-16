be84fec0147fee67fe07844623dc3d27ccfaa51a
jameson burchette <jameson.burchette@outlook.com>
2017-07-16 13:21:32 -0400

    add data access test class [#148966557]

---
+++ HousingManager.Data/HousingManager.Data.Tests/DataAccessTest.cs
@@ -0,0 +1,28 @@
+﻿using HousingManager.Data.Library.EFModels;
+using HousingManager.Data.Service;
+using HousingManager.Data.Service.Interfaces;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingManager.Data.Tests
+{
+    [TestFixture]
+    public class DataAccessTest
+    {
+        private IDataAccess<Address> _AdrData = new DataAccessEntityFactory().GetAddressDAO();
+
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
