6a9ac40ea46c557b0fc82f8115359f78a6b087c1
myapham <myapham0410@gmail.com>
2017-07-16 17:35:31 -0400

    Added contactinfo table under person [#148856741]

---
+++ HousingManager.DB/HousingManager.DB.SqlServer/Person/ContactInfo.sql
@@ -0,0 +1,7 @@
+﻿
+Create table Person.ContactInfo(
+	ContactInfoId int primary key clustered IDENTITY(1,1) not null,
+    EmailAddress nvarchar(50) not null,
+    PhoneNumber nvarchar(50) not null
+   
+);
