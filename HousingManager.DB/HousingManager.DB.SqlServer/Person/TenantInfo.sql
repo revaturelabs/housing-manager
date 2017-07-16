ca57817db420516c2cbbada11055286acbcc44ba
myapham <myapham0410@gmail.com>
2017-07-16 16:53:28 -0400

    Added TenantInfo table under person schema [#148835907]

---
+++ HousingManager.DB/HousingManager.DB.SqlServer/Person/TenantInfo.sql
@@ -0,0 +1,14 @@
+﻿Create table Person.TenantInfo(
+	TenantInfo int primary key clustered IDENTITY(1,1)not null,
+	PersonId int unique not null, 
+	BatchId int not null,
+	ApartmentUnitId int not null,
+	Gender nvarchar(20) not null,
+	MoveInDate datetime not null,
+	MoveOutDate datetime not null, 
+	HasCar bit,
+
+	CONSTRAINT FK_Person_TenantInfo_PersonId FOREIGN KEY (PersonId) REFERENCES Person.Person(PersonId),
+	CONSTRAINT FK_Person_TenantInfo_ApartmentUnitId FOREIGN KEY (ApartmentUnitId) REFERENCES Apartment.ApartmentUnit(ApartmentUnitId),
+	CONSTRAINT FK_Person_TenantInfo_BatchId FOREIGN KEY (BatchId) REFERENCES Batch.Batch(BatchId)
+);
