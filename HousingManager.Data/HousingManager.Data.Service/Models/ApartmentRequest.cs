1398bcfc9f7224406417d57d90db473be74117e2
Ryan Pacovsky <Ryan.Pacovsky@gmail.com>
2017-07-17 11:32:47 -0400

    Create the ApartmentRequest model. [#148925903]

---
+++ HousingManager.Data/HousingManager.Data.Service/Models/ApartmentRequest.cs
@@ -0,0 +1,16 @@
+﻿using HousingManager.Data.Library.EFModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingManager.Data.Service.Models
+{
+    public class ApartmentRequest
+    {
+        public DateTime Submitted { get; set; }
+        public int Beds { get; set; }
+        public Person Manager { get; set; }
+        public Provider Provider { get; set; }
+    }
+}
