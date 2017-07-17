2add96a094ac5093d4d95bf903dd8ce96cf726c3
Ryan Pacovsky <Ryan.Pacovsky@gmail.com>
2017-07-16 21:42:23 -0400

    Created the HouseKepping Model. [#148925363]

---
+++ HousingManager.Data/HousingManager.Data.Service/Models/HouseKeeping.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingManager.Data.Service.Models
+{
+    public class HouseKeeping
+    {
+        public ApartmentUnit ApartmentUnit { get; set; }
+        public DateTime LastCleaning { get; set; }
+        public DateTime NextCleaning { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null && obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            var o = obj as HouseKeeping;
+
+            return (o.ApartmentUnit == ApartmentUnit && o.LastCleaning == LastCleaning && o.NextCleaning == NextCleaning);
+        }
+
+        public override int GetHashCode()
+        {
+            return (ApartmentUnit.GetHashCode() + LastCleaning.GetHashCode() + NextCleaning.GetHashCode());
+        }
+
+        public ApartmentUnit GetApartmentUnit()
+        {
+            return ApartmentUnit;
+        }
+
+        public DateTime GetLastCleaning()
+        {
+            return LastCleaning;
+        }
+
+        public DateTime GetNextCleaning()
+        {
+            return NextCleaning;
+        }
+    }
+}
