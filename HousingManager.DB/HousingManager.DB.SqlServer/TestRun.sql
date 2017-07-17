c2a41ce3c8fe4a897af117f01cc3d9f72f345a2e
Yasha Taylor <yashataylor@gmail.com>
2017-07-17 00:09:55 -0400

    Added query for person table and test to insert people into table for front end use. [#148956373]

--- HousingManager.DB/HousingManager.DB.SqlServer/TestRun.sql
+++ HousingManager.DB/HousingManager.DB.SqlServer/TestRun.sql
@@ -17,5 +17,13 @@ GO
 
 SELECT * FROM Apartment.Address;
 
+SELECT * FROM Person.Person;
+
 INSERT INTO Apartment.Address(StreetName, AptNum, City, [State], ZipCode)
-values ('2121 Raven Tower Court', '504', 'Herndon', 'VA', 20170);
+values
+('2121 Raven Tower Court', '504', 'Herndon', 'VA', 20170);
+
+INSERT INTO Person.Person(FirstName, LastName)
+VALUES
+('Yasha', 'Taylor'),
+('Jameson', 'Burchette');
