0a8a40c0b31cdcbcfb277262a76c1ddfbc204494
myapham <myapham0410@gmail.com>
2017-07-16 20:16:18 -0400

    Added constraints for primarykey and foreign key to providersupplyrequest table [delivers #148972931]

--- HousingManager.DB/HousingManager.DB.SqlServer/Request/ProviderSupplyRequest.sql
+++ HousingManager.DB/HousingManager.DB.SqlServer/Request/ProviderSupplyRequest.sql
@@ -4,8 +4,7 @@
 	ProviderId int not null,
 	ManagerId int not null,
 	Submitted datetime,
-		CONSTRAINT FK_Request_ProviderSupplyRequest_SupplyId FOREIGN KEY (SupplyId) REFERENCES Request.Supply(SupplyId),
-		CONSTRAINT FK_Request_ProviderSupplyRequest_ManagerId FOREIGN KEY (ManagerId) REFERENCES Person.Person(PersonId),
-		CONSTRAINT FK_Request_ProviderSupplyRequest_ProviderId FOREIGN KEY (ProviderId) REFERENCES Person.Provider(ProviderId)
-
+	CONSTRAINT FK_Request_ProviderSupplyRequest_SupplyId FOREIGN KEY (SupplyId) REFERENCES Request.Supply(SupplyId),
+	CONSTRAINT FK_Request_ProviderSupplyRequest_ManagerId FOREIGN KEY (ManagerId) REFERENCES Person.Person(PersonId),
+	CONSTRAINT FK_Request_ProviderSupplyRequest_ProviderId FOREIGN KEY (ProviderId) REFERENCES Person.Provider(ProviderId)
 );
