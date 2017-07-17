e984bc6a0c3efb949c8c3a5645876d84092b5821
andre ballard <andre.ballard@outlook.com>
2017-07-17 11:36:22 -0400

    Created test methods for Batch model. Passed each test. [delivers #148998141]

--- HousingManager.Business/HousingManager.Business.Library/Models/Batch.cs
+++ HousingManager.Business/HousingManager.Business.Library/Models/Batch.cs
@@ -10,13 +10,12 @@ namespace HousingManager.Business.Library.Models
       public DateTime StartDate { get; set; }
       public DateTime EndDate { get; set; }
       public int size { get; set; }
-      public static List<Person> People = new List<Person>();
+      public List<Person> People = new List<Person>();
 
       public Person FindPerson(Person person)
       {
         return People.Find(p => p == person);
       }
 
-      
     }
 }
