c9ed1369ba12f1f801d24c25fe235807c58b3024
Daniel Larner <daniel.larner@outlook.com>
2017-07-17 19:52:23 -0400

    add properties to apartment complex model class [#149033703]

---
+++ HousingManager.Business/HousingManager.Business.Library/Models/ApartmentComplex.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingManager.Business.Library.Models
+{
+  public class ApartmentComplex
+  {
+    public string ApartmentName { get; set; }
+    public Address ApartmentAddress { get; set; }
+    public ApartmentUnit List<ApartmentUnit> { get; set; }
+  }
+}
