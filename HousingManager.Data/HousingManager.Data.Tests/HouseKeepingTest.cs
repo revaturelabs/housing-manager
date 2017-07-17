a8b22e5197469b9e6bdb27ab64e2c6e9e0b18660
Ryan Pacovsky <Ryan.Pacovsky@gmail.com>
2017-07-16 22:09:44 -0400

    Create the tests for the HouseKeeping Model. [delivers #148925363]

---
+++ HousingManager.Data/HousingManager.Data.Tests/HouseKeepingTest.cs
@@ -0,0 +1,104 @@
+﻿using HousingManager.Data.Service.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingManager.Data.Tests
+{
+    [TestFixture]
+    public class HouseKeepingTest
+    {
+        [Test]
+        public void HouseKeppingIsPositive()
+        {
+            var c = new HouseKeeping();
+            c.ApartmentUnit = new ApartmentUnit();
+            c.LastCleaning = new DateTime(2017, 10, 01);
+            c.NextCleaning = new DateTime(2017, 01, 10);
+            var c1 = new HouseKeeping();
+            c.ApartmentUnit = new ApartmentUnit();
+            c.LastCleaning = new DateTime(2017, 10, 01);
+            c.NextCleaning = new DateTime(2017, 01, 10);
+            var chash = c.GetHashCode();
+            var c1hash = c1.GetHashCode();
+
+            Assert.IsTrue(c.Equals(c1) && chash == c1hash);
+        }
+
+        [Test]
+        public void HouseKeepingIsNegative()
+        {
+            var c = new HouseKeeping();
+            c.ApartmentUnit = new ApartmentUnit();
+            c.LastCleaning = new DateTime(2017, 10, 01);
+            c.NextCleaning = new DateTime(2017, 01, 10);
+            var c1 = new HouseKeeping();
+            c.ApartmentUnit = new ApartmentUnit();
+            c.LastCleaning = new DateTime(2017, 10, 01);
+            c.NextCleaning = new DateTime(2017, 10, 10);
+            var chash = c.GetHashCode();
+            var c1hash = c1.GetHashCode();
+
+            Assert.IsFalse(c.Equals(c1) && chash == c1hash);
+        }
+
+        [Test]
+        public void ApartmentUnitPositive()
+        {
+            var n = new HouseKeeping();
+            n.ApartmentUnit = new ApartmentUnit();
+            var ApartmentUnit = n.GetApartmentUnit();
+
+            Assert.IsNotNull(ApartmentUnit);
+        }
+
+        [Test]
+        public void ApartmentUnitNegative()
+        {
+            var n = new HouseKeeping();
+            n.ApartmentUnit = new ApartmentUnit();
+            var ApartmentUnit = n.GetApartmentUnit();
+
+            Assert.IsNull(ApartmentUnit);
+        }
+
+        [Test]
+        public void LastCleaningPositive()
+        {
+            var n = new HouseKeeping();
+            n.LastCleaning = new DateTime(2017, 10, 01);
+            var LastCleaning = n.GetLastCleaning();
+
+            Assert.IsNotNull(LastCleaning);
+        }
+
+        [Test]
+        public void LastCleaningNegative()
+        {
+            var n = new HouseKeeping();
+            var LastCleaning = n.GetLastCleaning();
+
+            Assert.IsNotNull(LastCleaning);
+        }
+
+        [Test]
+        public void NextCleaningPositive()
+        {
+            var n = new HouseKeeping();
+            n.LastCleaning = new DateTime(2017, 10, 01);
+            var NextCleaning = n.GetNextCleaning();
+
+            Assert.IsNotNull(NextCleaning);
+        }
+
+        [Test]
+        public void NextCleaningNegative()
+        {
+            var n = new HouseKeeping();
+            var NextCleaning = n.GetNextCleaning();
+
+            Assert.IsNotNull(NextCleaning);
+        }
+    }
+}
