47b09eb5613a6fe865fd79c5fb90d49ff9a23d01
jameson burchette <jameson.burchette@outlook.com>
2017-07-17 09:23:28 -0400

    add the person controller class [#148777285]

---
+++ HousingManager.Business/HousingManager.Business.Service/Controllers/PersonController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HousingManager.Business.Service.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Person")]
+    public class PersonController : Controller
+    {
+        // GET: api/Person
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Person/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Person
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Person/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
