efa31f5dff6280365d1b93f88f8f349ca49bd54d
jameson burchette <jameson.burchette@outlook.com>
2017-07-17 00:41:42 -0400

    add person controller [#148774405]

--- HousingManager.Data/HousingManager.Data.Service/Controllers/PersonController.cs
+++ HousingManager.Data/HousingManager.Data.Service/Controllers/PersonController.cs
@@ -6,51 +6,49 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using HousingManager.Data.Service.Interfaces;
 using HousingManager.Data.Library.EFModels;
-using System.Net.Http;
-using Newtonsoft.Json;
 using HousingManager.Data.Service.DAOs;
 
 namespace HousingManager.Data.Service.Controllers
 {
     [Produces("application/json")]
-    [Route("api/Address")]
-    public class AddressController : Controller
+    [Route("api/Person")]
+    public class PersonController : Controller
     {
-        private IDataAccess<Address> _AdrData = DataAccessEntityFactory.GetAddressDAO();
+        private IDataAccess<Person> _PData = DataAccessEntityFactory.GetPersonDAO();
 
-        // GET: api/Address
+        // GET: api/Person
         [HttpGet]
-        public List<Address> Get()
+        public List<Person> Get()
         {
-            return _AdrData.Read();
+            return _PData.Read();
         }
 
-        // GET: api/Address/5
+        // GET: api/Person/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
-        
-        // POST: api/Address
+
+        // POST: api/Person
         [HttpPost]
-        public void Post([FromBody]Address adr)
+        public void Post([FromBody]Person per)
         {
-            _AdrData.Create(adr);
+            _PData.Create(per);
         }
-        
-        // PUT: api/Address/
+
+        // PUT: api/Person/
         [HttpPut]
-        public void Put([FromBody]Address adr)
+        public void Put([FromBody]Person per)
         {
             throw new NotImplementedException();
         }
-        
-        // DELETE: api/ApiWithActions/
+
+        // DELETE: api/Person/
         [HttpDelete]
-        public void Delete([FromBody]Address adr)
+        public void Delete([FromBody]Person per)
         {
-            _AdrData.Delete(adr);
+            _PData.Delete(per);
         }
     }
 }
