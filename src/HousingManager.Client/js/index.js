0f65656d3f8dfd3ef3abbacb24464386cf33ae1b
Yasha Taylor <yashataylor@gmail.com>
2017-07-17 09:48:30 -0400

    Finished adding button and formatted data to be more readible. [#148956373]

--- src/HousingManager.Client/js/index.js
+++ src/HousingManager.Client/js/index.js
@@ -1,20 +1,31 @@
 var angular = require('angular');
 var ngAddress = angular.module('ng-displayAddress', []);
+var ngPeople = angular.module('ng-displayPeople', []);
 
 ngAddress.controller('addressController', ['$scope', '$http', function($scope, $http) {
   $scope.getAddresses = function () {
-    $http.get('http://localhost:52709/api/Address').then(function (res) {
+    $http.get('http://localhost:52709/api/address').then(function (res) {
       $scope.addresses = res.data;
+
       count = 0;
       $scope.addresses.forEach(function(element) {
-        element.spot = count++;
+        element.counter = count++;
       });
     });
   };
+}]);
 
-  $scope.addressCounter = 1;
+ngPeople.controller('peopleController', ['$scope', '$http', function($scope, $http) {
+  $scope.getPeople = function () {
+    $http.get('http://localhost:52709/api/people').then(function (res) {
+      $scope.people = res.data;
 
-  $scope.incrementAddressCounter = function () {
-    $scope.addressCounter++;
+      count = 0;
+      $scope.people.forEach(function(element) {
+        element.counter = count++;
+      })
+    });
   };
 }]);
+
+
