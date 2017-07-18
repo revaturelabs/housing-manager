6625264c556ea2750998d69c6688eab0ba3bb519
Grant Taylor <grant.devlin.taylor@gmail.com>
2017-07-18 13:37:42 -0400

    Delivered Apartment Complex Controller with GetAll [delivers #148776611]

--- HousingManager.Business/HousingManager.Business.Service/Controllers/ApartmentComplexController.cs
+++ HousingManager.Business/HousingManager.Business.Service/Controllers/ApartmentComplexController.cs
@@ -7,23 +7,18 @@ using HousingManager.Business.Library.Models;
 
 namespace HousingManager.Business.Service.Brokers
 {
+    [Produces("application/json")]
+    [Route("api/ApartmentComplex")]
     public class ApartmentComplexController : Controller
     {
         ApartmentComplexBroker _broker = new ApartmentComplexBroker();
 
         [HttpGet]
-        public ApartmentComplex Get()
+        public IEnumerable<ApartmentComplex> GetAll( )
         {
-            return _broker.Get();
+            return _broker.GetAll();
         }
 
-
-        [HttpGet]
-        public IEnumerable<ApartmentComplex> GetAll()
-        {
-            return _broker.GetAll();    
-        }
-        
         
         [HttpPost]
         public void Post([FromBody]string value)
