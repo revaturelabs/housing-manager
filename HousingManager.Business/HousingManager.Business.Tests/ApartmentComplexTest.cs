cc458ebe58563ecc284b083a896c9f8946b01ba8
Daniel Larner <daniel.larner@outlook.com>
2017-07-18 12:09:01 -0400

    add test method for apartment complex broker get all method [finishes #149083225]

--- HousingManager.Business/HousingManager.Business.Tests/ApartmentComplexTest.cs
+++ HousingManager.Business/HousingManager.Business.Tests/ApartmentComplexTest.cs
@@ -1,4 +1,5 @@
-﻿using HousingManager.Business.Library.Models;
+﻿using HousingManager.Business.Library;
+using HousingManager.Business.Library.Models;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -17,6 +18,24 @@ namespace HousingManager.Business.Tests
       Assert.IsNotNull(ac.ApartmentName);
       Assert.IsNotNull(ac.ApartmentAddress);
       Assert.IsNotNull(ac.ApartmentUnit);
+    }
+
+    [Test]
+    public void TestApartmentComplexGetAll()
+    {
+      var a = new ApartmentComplexBroker();
+
+      List<ApartmentComplex> ac = new List<ApartmentComplex>();
+
+      ApartmentComplex apt = new ApartmentComplex();
+
+      apt.ApartmentName = "Test Dummy";
+
+      ac.Add(apt);
+
+      var aptcomp = a.GetAll(ac);
+
+      Assert.IsTrue(aptcomp[0].ApartmentName == "Test Dummy");
     }
   }
 }
