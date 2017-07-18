60220eddf7721611c46a98cd8f16894b30ac1b25
Grant Taylor <grant.devlin.taylor@gmail.com>
2017-07-18 15:48:13 -0400

    start added Tenant object to logic library [#149105593]

---
+++ HousingManager.Business/HousingManager.Business.Library/Models/Tenant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingManager.Business.Library.Models
+{
+    public class Tenant
+    {
+        Person _person = new Person();
+        
+        Person tenant {
+            get { return _person; }
+            set { _person = tenant; }
+        }
+
+        DateTime MoveIn { get; set; }
+        DateTime MoveOut { get; set; }
+
+
+        Address _address = new Address();
+        Address address
+        {
+            get { return _address; }
+            set { _address = address; }
+        }
+        
+        bool hasCar { get; set; }
+
+        string batchName { get; set; }
+    }
+}
