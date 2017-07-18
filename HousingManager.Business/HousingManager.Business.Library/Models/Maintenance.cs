ddc7630d001e7e16f6178fa6d475a0479de83be1
Daniel Larner <daniel.larner@outlook.com>
2017-07-18 14:39:58 -0400

    add maintenance model class to business logic project [#149103079]

---
+++ HousingManager.Business/HousingManager.Business.Library/Models/Maintenance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingManager.Business.Library.Models
+{
+  public class Maintenance
+  {
+    public string Issue { get; set; }
+    public ApartmentUnit ApartmentUnitNumber { get; set; }
+    public DateTime SubmissionDate { get; set; }
+  }
+}
---
+++ HousingManager.Business/HousingManager.Business.Tests/ModelTests/MaintenanceTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingManager.Business.Tests.ModelTests
+{
+  [TestFixture]
+  public class MaintenanceTest
+  {
+    [Test]
+    public 
+  }
+}
