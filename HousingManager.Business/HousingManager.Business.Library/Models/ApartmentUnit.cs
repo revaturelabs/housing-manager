ca24916adb0a83db0e6824e73eea93c4a2ee4ff6
Daniel Larner <daniel.larner@outlook.com>
2017-07-18 10:55:48 -0400

    create apartment unit model [#149082855]

---
+++ HousingManager.Business/HousingManager.Business.Library/Models/ApartmentUnit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingManager.Business.Library.Models
+{
+  public class ApartmentUnit
+  {
+    public int UnitNumber { get; set; }
+    public string ComplexName { get; set; }
+    public int Capacity { get; set; }
+    //public Provider UnitProvider { get; set; }
+  }
+}
