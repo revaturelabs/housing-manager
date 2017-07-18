73342c89a9cb2d95fb9fb27dc60d0c78bffc7952
Samuel Oates <samuel.oates1@gmail.com>
2017-07-18 10:29:20 -0400

    Wrote method to get the context of the database and access it. [#148775951]

--- HousingManager.Data/HousingManager.Data.Service/DAOs/DataAccessEntityFactory.cs
+++ HousingManager.Data/HousingManager.Data.Service/DAOs/DataAccessEntityFactory.cs
@@ -8,18 +8,23 @@ using HousingManager.Data.Library.EFModels;
 
 namespace HousingManager.Data.Service.DAOs
 {
-    public static class DataAccessEntityFactory
+  public static class DataAccessEntityFactory
+  {
+    private static HousingManager_DB_SqlServerContext _Context = new HousingManager_DB_SqlServerContext();
+
+    public static IDataAccess<Address> GetAddressDAO()
     {
-        private static HousingManager_DB_SqlServerContext _Context = new HousingManager_DB_SqlServerContext();
-
-        public static IDataAccess<Address> GetAddressDAO()
-        {
-            return new AddressDataAccess(_Context);
-        }
-
-        public static IDataAccess<Person> GetPersonDAO()
-        {
-            return new PersonDataAccess(_Context);
-        }
+      return new AddressDataAccess(_Context);
     }
+
+    public static IDataAccess<Person> GetPersonDAO()
+    {
+      return new PersonDataAccess(_Context);
+    }
+
+    public static IDataAccess<ApartmentComplex> GetApartmentComplexDAO()
+    {
+      return new ApartmentComplexAccess(_Context);
+    }
+  }
 }
