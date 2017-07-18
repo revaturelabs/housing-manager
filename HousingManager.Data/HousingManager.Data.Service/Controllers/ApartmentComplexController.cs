55336da4ae13f3b4264094e701c3f66f5a156184
Samuel Oates <samuel.oates1@gmail.com>
2017-07-18 10:40:10 -0400

    Create and get a list of apartmentcomplexes methods added to ApartmentComplexController. [#148775951]

---
+++ HousingManager.Data/HousingManager.Data.Service/Controllers/ApartmentComplexController.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HousingManager.Data.Service.Interfaces;
+using HousingManager.Data.Library.EFModels;
+using HousingManager.Data.Service.DAOs;
+
+namespace HousingManager.Data.Service.Controllers
+{
+  [Produces("application/json")]
+  [Route("api/ApartmentComplex")]
+  public class ApartmentComplexController : Controller
+  {
+
+    private IDataAccess<ApartmentComplex> _AptCpx = DataAccessEntityFactory.GetApartmentComplexDAO();
+
+    // GET: api/ApartmentComplex
+    [HttpGet]
+    public List<ApartmentComplex> Get()
+    {
+      return _AptCpx.Read();
+    }
+
+    // GET: api/ApartmentComplex/5
+    [HttpGet("{id}", Name = "Get")]
+    public string Get(int id)
+    {
+      return "value";
+    }
+
+    // POST: api/ApartmentComplex
+    [HttpPost]
+    public void Post([FromBody]ApartmentComplex aptcpx)
+    {
+      _AptCpx.Create(aptcpx);
+    }
+
+    // PUT: api/ApartmentComplex/5
+    [HttpPut("{id}")]
+    public void Put([FromBody]ApartmentComplex aptcpx)
+    {
+      throw new NotImplementedException();
+    }
+
+    // DELETE: api/ApiWithActions/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
