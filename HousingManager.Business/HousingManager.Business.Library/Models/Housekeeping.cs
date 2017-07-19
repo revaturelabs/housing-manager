adaefd2897b194e0074cffe34ddf00640b4b59f3
Daniel Larner <daniel.larner@outlook.com>
2017-07-19 14:42:16 -0400

    add housekeeping model class to the business logic project [finishes #149134829]

---
+++ HousingManager.Business/HousingManager.Business.Library/Models/Housekeeping.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingManager.Business.Library.Models
+{
+  public class Housekeeping
+  {
+    private DateTime _CurrentDate = new DateTime();
+    public DateTime CurrentDate
+    {
+      get
+      {
+        return _CurrentDate;
+      }
+      set
+      {
+        _CurrentDate = CurrentDate;
+      }
+    }
+
+    private DateTime _NextDate = new DateTime();
+    public DateTime NextDate
+    {
+      get
+      {
+        return _NextDate;
+      }
+      set
+      {
+        _NextDate = NextDate;
+      }
+    }
+  }
+}
---
+++ HousingManager.Business/HousingManager.Business.Tests/ModelTests/HousekeepingTests.cs
@@ -0,0 +1,22 @@
+﻿using HousingManager.Business.Library.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace HousingManager.Business.Tests.ModelTests
+{
+  [TestFixture]
+  public class HousekeepingTests
+  {
+    [Test]
+    public void HousekeepingModelTest()
+    {
+      var h = new Housekeeping();
+
+      Assert.IsNotNull(h.CurrentDate);
+      Assert.IsNotNull(h.NextDate);
+    }
+  }
+}
