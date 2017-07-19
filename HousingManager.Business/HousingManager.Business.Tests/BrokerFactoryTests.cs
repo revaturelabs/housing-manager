9314676746380d04ecb6777a924b64f2b47fc3e2
Grant Taylor <grant.devlin.taylor@gmail.com>
2017-07-19 15:35:20 -0400

    Delivered Broker Factory, tests return a real object when asking for GetInstance() [delivers #149183989]

---
+++ HousingManager.Business/HousingManager.Business.Tests/BrokerFactoryTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using HousingManager.Business.Library.Models;
+using HousingManager.Business.Service.Brokers;
+
+namespace HousingManager.Business.Tests
+{
+    [TestFixture]
+    public class BrokerFactoryTests
+    {
+        [Test]
+        public void CreateBrokerTest()
+        {
+            var TestBroker = BrokerFactory<Person>.GetInstance();
+
+            Assert.NotNull(TestBroker);
+        }
+
+    }
+}
