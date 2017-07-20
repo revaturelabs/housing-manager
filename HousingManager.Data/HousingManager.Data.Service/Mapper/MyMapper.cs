00d277f6a9af600da5d9f817e7973e7b0b7c4906
Jonathan <jonvette@comcast.net>
2017-07-20 09:25:08 -0400

    filled out mapper [#149185655]

--- HousingManager.Data/HousingManager.Data.Service/Mapper/MyMapper.cs
+++ HousingManager.Data/HousingManager.Data.Service/Mapper/MyMapper.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using HousingManager.Data.Library.EFModels;
+using HousingManager.Data.Service.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,12 @@ namespace HousingManager.Data.Service.Mapper
 {
     public class MyMapper
     {
+    private MapperConfiguration AddressEntity_AddressDTO = new MapperConfiguration(c => c.CreateMap<Address, AddressDTO>());
+
+
+    public T AddressEntitytoAddressDTO<T>(Address la)
+    {
+      return AddressEntity_AddressDTO.CreateMapper().Map<T>(la);
     }
+  }
 }
