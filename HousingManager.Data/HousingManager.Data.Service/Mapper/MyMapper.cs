792e9e852d9cdb67b04667825457fb05130d562b
Jonathan <jonvette@comcast.net>
2017-07-20 16:29:09 -0400

    Added methods to mapper [#149258871]

--- HousingManager.Data/HousingManager.Data.Service/Mapper/MyMapper.cs
+++ HousingManager.Data/HousingManager.Data.Service/Mapper/MyMapper.cs
@@ -8,14 +8,20 @@ using System.Threading.Tasks;
 
 namespace HousingManager.Data.Service.Mapper
 {
-    public class MyMapper
-    {
+  public class MyMapper
+  {
     private MapperConfiguration AddressEntity_AddressDTO = new MapperConfiguration(c => c.CreateMap<Address, AddressDTO>());
+    private MapperConfiguration PersonEntity_PersonDTO = new MapperConfiguration(c => c.CreateMap<Person, PersonDTO>());
 
 
     public T AddressEntitytoAddressDTO<T>(Address la)
     {
       return AddressEntity_AddressDTO.CreateMapper().Map<T>(la);
+    }
+
+    public T PersonEntitytoPersonDTO<T>(Person p)
+    {
+      return PersonEntity_PersonDTO.CreateMapper().Map<T>(p);
     }
   }
 }
