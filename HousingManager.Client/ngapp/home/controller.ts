ef8c7ba300fa99a871f6f38831931ea2c8113c56
Daniel Larner <daniel.larner@outlook.com>
2017-07-21 12:36:48 -0400

    Added the UI experience for Create Person [#149300115]

--- HousingManager.Client/ngapp/home/controller.ts
+++ HousingManager.Client/ngapp/home/controller.ts
@@ -1,4 +1,5 @@
 import { home as h } from './module';
+import * as ng from 'angular';
 import './service';
 
 class Entity {
@@ -50,7 +51,7 @@ class Address {
   }
 }
 
-h.controller('homeController', ['$scope', 'homeFactory', function ($scope, homeFactory) {
+h.controller('homeController', ['$scope', '$mdDialog', 'homeFactory', function ($scope, $mdDialog, homeFactory) {
   $scope.myAddress = new Address();
   $scope.myPerson = new Person();
   $scope.entities = [
@@ -63,5 +64,42 @@ h.controller('homeController', ['$scope', 'homeFactory', function ($scope, homeF
   }
   $scope.processPerson = function (id) {
     homeFactory.getPerson(id, $scope.myPerson);
-  }
+  } 
+
+  $scope.createPersonDialog = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'ngapp/home/partials/createPersonTemplate.html',
+      parent: ng.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:false,
+    })
+    .then(function(answer) {
+      $scope.status = answer + ' has been added.';
+    }, function() {
+      $scope.status = 'No one as been added.';
+    });
+  };
+
+  // $scope.createPerson = function(ev) {
+  //   $mdDialog.show(confirm).then(function(result) {
+  //     $scope.status = 'You decided to name your dog ' + result + '.';
+  //   }, function() {
+  //     $scope.status = 'You didn\'t name your dog.';
+  //   });
+  // };
+
+  function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+  };
 }]);
--- HousingManager.Client/ngapp/home/module.ts
+++ HousingManager.Client/ngapp/home/module.ts
@@ -1,5 +1,6 @@
 import * as ng from "angular";
 import 'file-loader?name=[name].[ext]&outputPath=ngapp/home/partials/!./partials/template.html';
+import 'file-loader?name=[name].[ext]&outputPath=ngapp/home/partials/!./partials/createPersonTemplate.html';
 
 var home = ng.module('ngHome', []);
 
