424544937246341100ae1ed01baadb49b68aeb0a
Daniel Larner <daniel.larner@outlook.com>
2017-07-24 15:01:37 -0400

    add guid to contact info model [#149434783]

--- HousingManager.Business/HousingManager.Business.Library/Models/ContactInfo.cs
+++ HousingManager.Business/HousingManager.Business.Library/Models/ContactInfo.cs
@@ -7,36 +7,37 @@ using System.Text;
 
 namespace HousingManager.Business.Library.Models
 {
-    public class ContactInfo : IModel
+  public class ContactInfo : IModel
+  {
+    public string EmailAddress { get; set; }     
+    public string PhoneNumber { get; set; }
+    public Guid Guid { get; set; }
+
+    public override bool Equals(object obj)
     {
-        public string EmailAddress { get; set; }     
-        public string PhoneNumber { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            if (obj == null && obj.GetType() != GetType())
-            {
-              return false;
-            }
+      if (obj == null && obj.GetType() != GetType())
+      {
+        return false;
+      }
             
-            var o = obj as ContactInfo;
+      var o = obj as ContactInfo;
            
-            return (o.EmailAddress == EmailAddress && o.PhoneNumber == PhoneNumber);
-        }
-
-        public override int GetHashCode()
-        {
-          return (EmailAddress.GetHashCode() + PhoneNumber.GetHashCode());
-        }
-
-        public string GetEmail()
-        {
-            return EmailAddress;
-        }
-
-        public string GetPhoneNumber()
-        {
-            return PhoneNumber;
-        }
+      return (o.EmailAddress == EmailAddress && o.PhoneNumber == PhoneNumber);
     }
+
+    public override int GetHashCode()
+    {
+      return (EmailAddress.GetHashCode() + PhoneNumber.GetHashCode());
+    }
+
+    public string GetEmail()
+    {
+      return EmailAddress;
+    }
+
+    public string GetPhoneNumber()
+    {
+      return PhoneNumber;
+    }
+  }
 }
