1e23cbd755ec148d6a34e1c4c7fa1a10d210bb57
Jonathan <jonvette@comcast.net>
2017-07-25 16:43:59 -0400

    added information to post method to add a person to a batch. Added some mock code for the rest of the information [#149525079]

---
+++ HousingManager.Data/HousingManager.Data.Service/DTOs/PersonBatchDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingManager.Data.Service.DTOs
+{
+    public class PersonBatchDTO
+    {
+
+    public Guid PersonGuid { get; set; }
+    public Guid BatchGuid { get; set; }
+    public DateTime MoveInDate { get; set; }
+    public DateTime MoveOutDate { get; set; }
+    public string Gender { get; set; }
+
+    }
+}
