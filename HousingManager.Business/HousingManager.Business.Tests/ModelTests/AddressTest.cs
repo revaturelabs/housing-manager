a72d96a53a76fd276dbea48059a4f3351446a49d
Daniel Larner <daniel.larner@outlook.com>
2017-07-25 16:07:19 -0400

    add methods to housekeeping, maintenance, and person models [#149513911]

--- HousingManager.Business/HousingManager.Business.Tests/ModelTests/AddressTest.cs
+++ HousingManager.Business/HousingManager.Business.Tests/ModelTests/AddressTest.cs
@@ -7,25 +7,9 @@ using HousingManager.Business.Library.Models;
 
 namespace HousingManager.Business.Tests
 {
-    [TestFixture]
-    public class AddressTest
-    {
-        //[Test]
-        //public void TestAddressModelSuccess()
-        //{
-        //    //var a = new Broker();
-        //    var address = a.AddAddress();
-        //    Assert.IsNotNull(address);   
-        //}
-
-        //[Test]
-        //public void TestAddressModelFail()
-        //{
-        //   // var a = new Broker();
-        //    var address = a.AddAddress2();
-        //    Assert.IsNull(address);
-        //}
-
+  [TestFixture]
+  public class AddressTest
+  {
     [Test]
     public void ContactInfoIsEqualPositive()
     {
