70f28e9d6d934561731a8c4ca5b1dd28ecb1e0ae
Daniel Larner <daniel.larner@outlook.com>
2017-07-27 18:06:01 -0400

    add validate person method tests to test project [#149304917]

---
+++ HousingManager.Business/HousingManager.Business.Tests/HousingManagerTests/HousingManagerLibraryTests.cs
@@ -0,0 +1,35 @@
+﻿using HousingManager.Business.Library.Managers;
+using HousingManager.Business.Library.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingManager.Business.Tests
+{
+  [TestFixture]
+  public class HousingManagerLibraryTests
+  {
+    [Test]
+    public void ValidatePersonMethodTestPos()
+    {
+      Person p = new Person();
+
+      p.FirstName = "Daniel";
+      p.LastName = "Larner";
+
+      Assert.IsTrue(HousingManagerLibrary.ValidatePerson(p));
+    }
+
+    [Test]
+    public void ValidatePersonMethodTestNeg()
+    {
+      Person p = new Person();
+
+      p.FirstName = "";
+      p.LastName = "string";
+
+      Assert.IsFalse(HousingManagerLibrary.ValidatePerson(p));
+    }
+  }
+}
